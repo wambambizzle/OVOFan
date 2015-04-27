1fb527151e6df74e51b9d533e26408784582ff43
Jordan Anderson <mr.janderson14@gmail.com>
2015-04-27 11:56:42 -0400

    init commit of xcode project

---
+++ OVOFan/OVOFan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OVOFan
+//
+//  Created by Jordan Anderson on 4/27/15.
+//  Copyright (c) 2015 Jordan Anderson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
